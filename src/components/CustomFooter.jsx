cdc427f9e4f23d37ed1849714c63fbe224ccf83f
kevinthomas <kevvin.thomas@gmail.com>
2023-12-12 10:30:42 +0530

    ui touch ups

--- src/components/CustomFooter.jsx
+++ src/components/CustomFooter.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CustomFooter = () => {
   return (
-    <div className='bg-stone-950 text-stone-500 font-dgs w-screen py-3 text-center items-center'>
+    <div className='bg-stone-950 text-stone-500 font-dgs w-screen py-3 px-3 text-center items-center'>
         Made in a hostel room while contemplating my engineering degree.
         <h1>🇮🇳</h1>
     </div>
