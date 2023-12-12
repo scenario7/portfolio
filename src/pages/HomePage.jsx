189d2b435989044b9cc38d43bc3e0541ac2356aa
kevinthomas <kevvin.thomas@gmail.com>
2023-12-12 10:32:34 +0530

    ui touch ups

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ import ContactCard from '../components/ContactCard'
 
 const HomePage = () => {
   return (
-    <div className='flex max-w-fit flex-col items-start px-5'>
+    <div className='flex max-w-fit flex-col items-start px-5 bg-gradient-to-t from-black to-[#101010]'>
         <div className='w-full flex flex-col items-center space-y-5'>
             <NavBar/>
             <HeroSection/>
