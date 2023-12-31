e90d5cb3117faf62fc414034d0536e4606daf4dc
kevinthomas <kevvin.thomas@gmail.com>
2023-12-31 14:33:43 +0530

    added icons

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FaLinkedin, FaGithub, FaTwitter, FaBehance, FaEnvelope } from 'react-icons/fa'
+import { FaXTwitter } from 'react-icons/fa6'
 
 const ContactCard = () => {
   return (
@@ -7,30 +9,30 @@ const ContactCard = () => {
             <h1 className='text-white font-dgs font-semibold text-3xl'>Contact</h1>
             <h1 className='text-stone-300 font-dgs font-regular text-xl'>Need some work done? Reach out to me!</h1>
         </div>
-        <div className="flex flex-col items-center lg:items-end md:items-end py-5 text-lg font-semibold">
+        <div className="flex items-center lg:items-end md:items-end py-5 gap-2 text-lg font-semibold">
             <a href="https://linkedin.com/in/kevvinthomas" target='_blank'>
-                <button className='text-blue-300 font-dgs'>
-                    LinkedIn
+                <button className='text-white font-dgs'>
+                    <FaLinkedin/>
                 </button>
             </a>
             <a href="https://github.com/scenario7" target='_blank'>
-                <button className='text-stone-300 font-dgs'>
-                    GitHub
+                <button className='text-white font-dgs'>
+                    <FaGithub/>
                 </button>
             </a>
             <a href="mailto:kevvin.thomas@gmail.com" target='_blank'>
-                <button className='text-red-300 font-dgs'>
-                    Email
+                <button className='text-white font-dgs'>
+                    <FaEnvelope/>
                 </button>
             </a>
             <a href="https://behance.net/kevvinthomas" target='_blank'>
-                <button className='text-blue-100 font-dgs'>
-                    Behance
+                <button className='text-white font-dgs'>
+                    <FaBehance/>
                 </button>
             </a>
             <a href="https://twitter.com/_kevinstappen" target='_blank'>
                 <button className='text-white font-dgs'>
-                    Twitter
+                    <FaXTwitter/>
                 </button>
             </a>
         </div>
